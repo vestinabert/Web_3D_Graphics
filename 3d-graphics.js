61e871e4b3e417b3394c8b2464106c15eda911dc
Vestina Bertašiūtė <127593981+vestinabert@users.noreply.github.com>
2023-09-06 12:03:50 +0300

    Creating a skybox

--- 3d-graphics.js
+++ 3d-graphics.js
@@ -14,5 +14,15 @@ var createScene = function () {
     // Add a hemispheric light for ambient lighting
     var light2 = new BABYLON.HemisphericLight("hemiLight", new BABYLON.Vector3(0, -2, 0), scene);
 
+    // Skybox creation and material setup
+    var skybox = BABYLON.MeshBuilder.CreateBox("skyBox", { size: 1000.0 }, scene);
+    var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
+    skyboxMaterial.backFaceCulling = false;
+    skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("textures/skybox", scene);
+    skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+    skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
+    skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
+    skybox.material = skyboxMaterial;
+    
     return scene;
 };
